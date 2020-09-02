8cbc62e51f78afa65408b8ee971139b74f742f9d
Hudson Duarte <hudsoneeto@outlook.com>
2020-09-02 17:28:26 -0300

    autenticação de usuário

---
+++ backend/iniciando-back-end/src/services/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import {getRepository} from 'typeorm'
+import { compare } from 'bcryptjs'
+import { sign, verify } from 'jsonwebtoken'
+
+import User from '../models/User'
+
+interface Request {
+    email: string;
+    password: string;
+}
+
+interface Response {
+    user: User,
+    token: string,
+}
+
+class AuthenticateUserService {
+    public async execute({email, password}: Request): Promise<Response>{
+        const usersRepository = getRepository(User);
+
+        const user = await usersRepository.findOne({ where: { email  } });
+
+        if(!user){
+            throw new Error('Incorrect email/password combination');
+        }
+
+        const passwordMatched = await compare(password, user.password);
+
+        if(!passwordMatched){
+            throw new Error('Incorrect email/password combination');
+        }
+
+        const token = sign({}, 'f56cc10361168167f644d0a43c8e9f87', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return {
+            user,
+            token,
+        }
+        
+    }
+}
+
+export default AuthenticateUserService;
