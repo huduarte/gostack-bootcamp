67c67fa7e499e3f8c12688874ffbb8ec07366241
Hudson Duarte <hudsoneeto@outlook.com>
2020-09-03 16:20:24 -0300

    autenticação

--- backend/iniciando-back-end/src/routes/appointments.routes.ts
+++ backend/iniciando-back-end/src/routes/appointments.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm'
+
+import ensureAuthenticaded from '../middlewares/ensureAuthenticated';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
+appointmentsRouter.use(ensureAuthenticaded);
+
 
 appointmentsRouter.get('/', async (request, response) => {
+    console.log(request.user)
+
     const appointmentsRepository = getCustomRepository(AppointmentsRepository)
     const appointments = await appointmentsRepository.find();
 
--- backend/iniciando-back-end/src/services/AuthenticateUserService.ts
+++ backend/iniciando-back-end/src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import {getRepository} from 'typeorm'
 import { compare } from 'bcryptjs'
 import { sign, verify } from 'jsonwebtoken'
 
+import authConfig from '../config/auth'
 import User from '../models/User'
 
 interface Request {
@@ -30,9 +31,11 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination');
         }
 
-        const token = sign({}, 'f56cc10361168167f644d0a43c8e9f87', {
+        const {secret, expiresIn} = authConfig.jwt;
+
+        const token = sign({}, secret , {
             subject: user.id,
-            expiresIn: '1d',
+            expiresIn,
         });
 
         return {
