a560f7e1ef7339fbd199a746af401ea943b69c21
Hudson Duarte <hudsoneeto@outlook.com>
2020-09-04 15:29:06 -0300

    Tratativa de erros

--- backend/iniciando-back-end/src/services/UpdateUserAvatarService.ts
+++ backend/iniciando-back-end/src/services/UpdateUserAvatarService.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 
 import User from '../models/User';
 import UploadConfig from '../config/upload';
-
+import AppError from '../errors/AppError';
 
 interface Request{
     user_id: string;
@@ -19,7 +19,7 @@ class UpdateUserAvatarService{
         const user = await usersRepository.findOne(user_id);
 
         if(!user){
-            throw new Error('Only authenticated users can change avatar.')
+            throw new AppError('Only authenticated users can change avatar.', 401)
         }
 
         if(user.avatar){
