1bd212390c16f9bc7a3fa93db8846c8e4045eb3a
Hudson Duarte <hudsoneeto@outlook.com>
2020-10-10 07:56:25 -0300

    TDD

--- backend/gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ backend/gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,19 +1,65 @@
 import AppError from '@shared/errors/AppError';
-import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import FakeUsersRepository from '../repositories/Fakes/FakeUsersRepository';
+import FakeUserTokenRepository from '../repositories/Fakes/FakeUserTokenRepository';
+
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
 
+let fakeUsersRepository: FakeUsersRepository;
+let fakeMailProvider: FakeMailProvider;
+let fakeUserTokenRepository: FakeUserTokenRepository;
+let sendForgotPasswordEmail: SendForgotPasswordEmailService;
+
 describe('SendForgotPasswordEmail', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeMailProvider = new FakeMailProvider();
+    fakeUserTokenRepository = new FakeUserTokenRepository();
+
+    sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+      fakeUserTokenRepository,
+    );
+  });
+
   it('should be able to recover the password using the email', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-
-    const createUser = new SendForgotPasswordEmailService(fakeUsersRepository);
-
-    await createUser.execute({
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+    await fakeUsersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
     });
 
-    expect(user).toHaveProperty('id');
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+
+  it('should not be able to recover the password with a non-existing user', async () => {
+    await expect(
+      sendForgotPasswordEmail.execute({
+        email: 'johndoe@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should generate a forgot password token', async () => {
+    const generateToken = jest.spyOn(fakeUserTokenRepository, 'generate');
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(generateToken).toHaveBeenCalledWith(user.id);
   });
 });
--- backend/gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/gobarber-backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,10 @@
 import { injectable, inject } from 'tsyringe';
 
 // import AppError from '@shared/errors/AppError';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 // import User from '../infra/typeorm/entities/User';
 
@@ -14,9 +17,28 @@ class SendForgotPassordEmailService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
-  public async execute({}: IRequest): Promise<void> {}
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exist');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
 }
 
 export default SendForgotPassordEmailService;
