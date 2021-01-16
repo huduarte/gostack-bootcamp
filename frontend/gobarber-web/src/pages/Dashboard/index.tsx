460c9c8a32620cb4519b26808980309dfc931ae7
Hudson <hudsoneeto@outlook.com>
2021-01-16 09:42:32 -0300

    [cod] Componente de proximo agendamento

--- frontend/gobarber-web/src/pages/Dashboard/index.tsx
+++ frontend/gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-import { Container, Header, HeaderContent, Profile } from './styles'
+import 
+{ 
+  Container, 
+  Header, 
+  HeaderContent, 
+  Profile, 
+  Content, 
+  Schedule, 
+  NextAppointment,
+  Calendar,
+} 
+from './styles'
 
 import logoImg from '../../assets/logo.svg'
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
@@ -39,7 +50,34 @@ const Dashboard: React.FC = () => {
         </button>
       </HeaderContent>
     </Header>
+    <Content>
+      <Schedule>
+        <h1>Horários agendados</h1>
+        <p>
+          <span>Hoje</span>
+          <span>Dia 06</span>
+          <span>Segunda-feira</span>
+        </p>
+
+        <NextAppointment>
+          <strong>Atendimento a seguir:</strong>
+          <div>
+            <img 
+              src="https://avatars1.githubusercontent.com/u/50152238?s=460&u=bf2ede60a8376e0b2443b5189f8b5df3699f8b56&v=4" 
+              alt=""/>
+
+              <strong>Diego Fernandes</strong>
+              <span>
+                <FiClock/>
+                08:00
+              </span>
+          </div>
+        </NextAppointment>
+      </Schedule>
+      <Calendar />
+    </Content>
   </Container>
+  
   )
 };
 
