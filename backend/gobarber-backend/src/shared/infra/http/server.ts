8e89b89eb88b966415bbd02922a1d4942be0f4e5
Hudson <hudsoneeto@outlook.com>
2021-01-17 08:53:45 -0300

    [fix] movendo o rateLimiter para depois da rota de files

--- backend/gobarber-backend/src/shared/infra/http/server.ts
+++ backend/gobarber-backend/src/shared/infra/http/server.ts
@@ -15,10 +15,10 @@ import '@shared/container';
 
 const app = express();
 
-app.use(rateLmiter)
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLmiter)
 app.use(routes);
 
 app.use(errors());
