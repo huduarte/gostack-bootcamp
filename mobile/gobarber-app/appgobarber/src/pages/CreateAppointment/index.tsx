04247029d4bb7ecc6b58b9443fe70d60abcd4e67
Hudson <hudsoneeto@outlook.com>
2021-01-21 07:11:15 -0300

    [cod] mostrando os horarios em tela

--- mobile/gobarber-app/appgobarber/src/pages/CreateAppointment/index.tsx
+++ mobile/gobarber-app/appgobarber/src/pages/CreateAppointment/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather'
 import { Platform } from 'react-native'
+import { format } from 'date-fns'
 import DateTimePicker from '@react-native-community/datetimepicker'
 
 import api from '../../services/api';
@@ -13,6 +14,7 @@ import
   BackButton,
   HeaderTitle,
   UserAvatar,
+  Content,
   ProvidersListContainer,
   ProvidersList,
   ProviderContainer,
@@ -22,10 +24,21 @@ import
   Title,
   OpenDatePickerButton,
   OpenDatePickerButtonText,
+  Schedule,
+  Section,
+  SectionTitle,
+  SectionContent,
+  Hour,
+  HourText,
 } from './styles'
 
 interface RouteParams {
   providerId: string;
+}
+
+interface AvailabilityItem {
+  hour: number;
+  available: boolean
 }
 
 const CreateAppointment: React.FC = () => {
@@ -34,16 +47,30 @@ const CreateAppointment: React.FC = () => {
   const { goBack } = useNavigation();
   const routeParams = route.params as RouteParams;
 
+  const [availability, setAvailability] = useState<AvailabilityItem[]>([]);
   const [providers, setProviders] = useState<Provider[]>([])
-  const [selectedProvider, setSelectedProvider] = useState(routeParams.providerId);
   const [showDatePicker, setShowDatePicker] = useState(false)
   const [selectedDate, setSelectedDate] = useState(new Date());
-
+  const [selectedHour, setSelectedHour] = useState(0);
+  const [selectedProvider, setSelectedProvider] = useState(routeParams.providerId);
+  
   useEffect(() => {
     api.get('providers').then(response => {
       setProviders(response.data);
     })
   }, [])
+
+  useEffect(() => {
+    api.get(`/providers/${selectedProvider}/day-availability`,{
+      params: {
+        year: selectedDate.getFullYear(),
+        month: selectedDate.getMonth() + 1,
+        day: selectedDate.getDate(),
+      }
+    }).then(response => {
+      setAvailability(response.data);
+    })
+  }, [selectedDate, selectedProvider])
 
   const navigateBack = useCallback(() => {
     goBack();
@@ -68,6 +95,34 @@ const CreateAppointment: React.FC = () => {
       }
   }, [])
 
+  const morningAvailabiity = useMemo(() => {
+    return availability
+      .filter(({ hour }) => hour < 12)
+      .map(({ hour, available}) => {
+        return {
+          hour,
+          available,
+          hourFormatted: format(new Date().setHours(hour), 'HH:00'),
+        }
+      })
+  }, [availability])
+
+  const afternoonAvailability = useMemo(() => {
+    return availability
+      .filter(({ hour }) => hour >= 12)
+      .map(({ hour, available}) => {
+        return {
+          hour,
+          available,
+          hourFormatted: format(new Date().setHours(hour), 'HH:00'),
+        }
+      })
+  }, [availability])
+
+  const handleSelectHour = useCallback((hour: number) => {
+    setSelectedHour(hour)
+  },[])
+
   return (
     <Container>
       <Header>
@@ -80,48 +135,91 @@ const CreateAppointment: React.FC = () => {
         <UserAvatar source={{uri: user.avatar_url ? user.avatar_url : 'https://premierpesa.co.ke/public/assets/img/anonymous.png'}}/>
       </Header>
 
-      <ProvidersListContainer>
-        <ProvidersList
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          data={providers}
-          keyExtractor={provider => provider.id}
-          renderItem={({item: provider}) => (
-            <ProviderContainer
-              onPress={() => handleSelectProvider(provider.id)}
-              selected={provider.id === selectedProvider}
-            >
-              <ProviderAvatar 
-                source={{uri: provider.avatar_url ? provider.avatar_url : 'https://premierpesa.co.ke/public/assets/img/anonymous.png'}}/>
-              <ProviderName
+      <Content>
+        <ProvidersListContainer>
+          <ProvidersList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={providers}
+            keyExtractor={provider => provider.id}
+            renderItem={({item: provider}) => (
+              <ProviderContainer
+                onPress={() => handleSelectProvider(provider.id)}
                 selected={provider.id === selectedProvider}
               >
-                {provider.name}
-              </ProviderName>
-            </ProviderContainer>
-          )}
-        />
-      </ProvidersListContainer>
+                <ProviderAvatar 
+                  source={{uri: provider.avatar_url ? provider.avatar_url : 'https://premierpesa.co.ke/public/assets/img/anonymous.png'}}/>
+                <ProviderName
+                  selected={provider.id === selectedProvider}
+                >
+                  {provider.name}
+                </ProviderName>
+              </ProviderContainer>
+            )}
+          />
+        </ProvidersListContainer>
 
-      <Calendar>
-        <Title>
-          Escolha a data
-        </Title>
+        <Calendar>
+          <Title>
+            Escolha a data
+          </Title>
 
-        <OpenDatePickerButton onPress={handleToggleDatePicker}>
-            <OpenDatePickerButtonText>
-              Selecionar outra data
-            </OpenDatePickerButtonText>
-        </OpenDatePickerButton>
+          <OpenDatePickerButton onPress={handleToggleDatePicker}>
+              <OpenDatePickerButtonText>
+                Selecionar outra data
+              </OpenDatePickerButtonText>
+          </OpenDatePickerButton>
 
-         {showDatePicker && (<DateTimePicker
-          mode="date"
-          display="calendar"
-          textColor="#f4ede8"
-          onChange={handleDateChanged}
-          value={selectedDate}
-          />)}
-      </Calendar>
+          {showDatePicker && (<DateTimePicker
+            mode="date"
+            display="calendar"
+            textColor="#f4ede8"
+            onChange={handleDateChanged}
+            value={selectedDate}
+            />)}
+        </Calendar>
+
+        <Schedule>
+          <Title>Escolha o horário</Title>
+
+          <Section>
+            <SectionTitle>Manhã</SectionTitle>
+
+            <SectionContent>
+              {morningAvailabiity.map(({hourFormatted, hour, available }) => (
+                <Hour 
+                  enabled={available}
+                  selected={selectedHour === hour}
+                  available={available} 
+                  key={hourFormatted}
+                  onPress={() => handleSelectHour(hour)}
+                >
+                  <HourText selected={selectedHour === hour}>{hourFormatted}</HourText>
+                </Hour>
+              ))}
+            </SectionContent>
+          </Section>
+
+          <Section>
+            <SectionTitle>Tarde</SectionTitle>
+
+            <SectionContent>
+              {afternoonAvailability.map(({hourFormatted, hour, available }) => (
+                  <Hour
+                    enabled={available}
+                    selected={selectedHour === hour}
+                    available={available} 
+                    key={hourFormatted}
+                    onPress={() => handleSelectHour(hour)}
+                  >
+                    <HourText selected={selectedHour === hour}>{hourFormatted}</HourText>
+                  </Hour>
+                ))}
+            </SectionContent>
+          </Section>
+        </Schedule>
+      </Content>
+
     </Container>
   );
 };
