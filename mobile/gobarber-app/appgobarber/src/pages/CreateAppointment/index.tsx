efdb9c9c81e45bb6a04f8f05d3f278396cf974af
Hudson <hudsoneeto@outlook.com>
2021-01-21 07:42:25 -0300

    [cod] fluxo de agendamento completo

--- mobile/gobarber-app/appgobarber/src/pages/CreateAppointment/index.tsx
+++ mobile/gobarber-app/appgobarber/src/pages/CreateAppointment/index.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather'
-import { Platform } from 'react-native'
+import { Platform, Alert } from 'react-native'
 import { format } from 'date-fns'
 import DateTimePicker from '@react-native-community/datetimepicker'
 
@@ -30,6 +30,8 @@ import
   SectionContent,
   Hour,
   HourText,
+  CreateAppointmentButton,
+  CreateAppointmentButtonText,
 } from './styles'
 
 interface RouteParams {
@@ -44,7 +46,7 @@ interface AvailabilityItem {
 const CreateAppointment: React.FC = () => {
   const { user } = useAuth();
   const route = useRoute();
-  const { goBack } = useNavigation();
+  const { goBack, navigate } = useNavigation();
   const routeParams = route.params as RouteParams;
 
   const [availability, setAvailability] = useState<AvailabilityItem[]>([]);
@@ -122,6 +124,28 @@ const CreateAppointment: React.FC = () => {
   const handleSelectHour = useCallback((hour: number) => {
     setSelectedHour(hour)
   },[])
+
+  const handleCreateAppointment = useCallback(async () => {
+    try{
+      const date = new Date(selectedDate);
+
+      date.setHours(selectedHour);
+      date.setMinutes(0);
+
+      await api.post('appointments', {
+        provider_id: selectedProvider,
+        date,
+      })
+
+      navigate('AppointmentCreated', { date: date.getTime() })
+    } catch (err) {
+      Alert.alert(
+        'Erro ao criar agendamento',
+        'Ocorreu um erro ao tentar criar o agendamento, tente novamente.'
+      )
+    }
+
+  }, [navigate, selectedDate, selectedHour, selectedProvider])
 
   return (
     <Container>
@@ -218,6 +242,10 @@ const CreateAppointment: React.FC = () => {
             </SectionContent>
           </Section>
         </Schedule>
+
+        <CreateAppointmentButton onPress={handleCreateAppointment}>
+          <CreateAppointmentButtonText>Agendar</CreateAppointmentButtonText>
+        </CreateAppointmentButton>
       </Content>
 
     </Container>
--- mobile/gobarber-app/appgobarber/src/pages/CreateAppointment/styles.ts
+++ mobile/gobarber-app/appgobarber/src/pages/CreateAppointment/styles.ts
@@ -86,9 +86,8 @@ export const ProviderName = styled.Text<ProviderNameProps>`
     color: ${props => props.selected ? '#232129' : '#f4ede8'};
 `;
 
-export const Calendar = styled.View`
+export const Calendar = styled.View``;
 
-`;
 export const Title = styled.Text`
     font-family: 'RobotoSlab-Medium';
     color: #f4ede8;
@@ -145,4 +144,19 @@ export const HourText = styled.Text<HourTextProps>`
     color: ${(props) => (props.selected ? '#232129' : '#f4ede8')};
     font-family: 'RobotoSlab-Regular';
     font-size: 16px;
+`;
+
+export const CreateAppointmentButton = styled(RectButton)`
+    height: 50px;
+    background: #ff9000;
+    border-radius: 10px;
+    align-items: center;
+    justify-content: center;
+    margin: 0 24px 24px;
+`;
+
+export const CreateAppointmentButtonText = styled.Text`
+    font-family: 'RobotoSlab-Medium';
+    font-size: 18px;
+    color: #232139;
 `;
